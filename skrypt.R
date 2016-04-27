8df73b04e4a08282145362c2f86f71ccdbc7f2f4
DawKub <kubow15@o2.pl>
2016-04-27 20:04:11 +0200

    zmiana deklaracji

--- skrypt.R
+++ skrypt.R
@@ -21,6 +21,12 @@ wspolczynnik_zmiennosci <- function(odch, sred)
   return(v)
 }
 
+wsk_asymetrii<- function(sr,moda)
+{
+  result<- sr-moda
+  return(result)
+}
+
 dane_sklepu_1 <- read.table("sklep1.txt", header=F, dec=",")
 dane_sklepu_2 <- read.table("sklep2.txt", header=F, dec=",")
 
@@ -32,13 +38,19 @@ dane_sklepu2_vec <- c(dane_sklepu_2[[1]])
 sort(dane_sklepu2_vec)
 
 sklep1_sr <- mean(dane_sklepu1_vec)
-cat("Srednia sklepu 1: ", sklep1_sr)
 sklep1_q1 <- quantile(dane_sklepu1_vec, 0.25)
+sklep1_q3 <- quantile(dane_sklepu1_vec, 0.75)
+sklep1_odch <- sd(dane_sklepu1_vec)
+
+sklep2_sr <- mean(dane_sklepu2_vec)
+sklep2_q1<-quantile(dane_sklepu2_vec, 0.25)
+sklep2_q3<-quantile(dane_sklepu2_vec, 0.75)
+sklep2_odch <- sd(dane_sklepu2_vec)
+
+cat("Srednia sklepu 1: ", sklep1_sr)
 cat("Kwartyl 0.25 sklepu 1:", sklep1_q1)
 cat("Mediana sklepu 1: ", median(dane_sklepu1_vec))
-sklep1_q3 <- quantile(dane_sklepu1_vec, 0.75)
 cat("Kwartyl 0.75 sklepu 1:", sklep1_q3)
-sklep1_odch <- sd(dane_sklepu1_vec)
 cat("Odchylenie standardowe nieobciazone sklepu 1: ", sklep1_odch)
 cat("Odchylenie cwiartkowe sklepu 1: ", cwiartkowe(sklep1_q1,sklep1_q3))
 cat("Wariancja nieobciazona sklepu 1: ", var(dane_sklepu1_vec))
@@ -46,14 +58,11 @@ cat("Dominanta sklepu 1: ", moda(dane_sklepu1_vec))
 cat("Rozstep sklepu 1: ", rozstep(dane_sklepu1_vec))
 cat("Wspolczynnik zmiennosci sklepu 1: ", wspolczynnik_zmiennosci(sklep1_sr, sklep1_odch), "%")
 
-sklep2_sr <- mean(dane_sklepu2_vec)
+
 cat("Srednia sklepu 2: ", sklep2_sr)
-sklep2_q1<-quantile(dane_sklepu2_vec, 0.25)
 cat("Kwartyl 0.25 sklepu 2:", sklep2_q1 )
 cat("Mediana sklepu 2: ", median(dane_sklepu2_vec))
-sklep2_q3<-quantile(dane_sklepu2_vec, 0.75)
 cat("Kwartyl 0.75 sklepu 2:", sklep2_q3)
-sklep2_odch <- sd(dane_sklepu2_vec)
 cat("Odchylenie standardowe nieobciazone sklepu 2: ", sklep2_odch)
 cat("Odchylenie cwiartkowe sklepu 2: ", cwiartkowe(sklep2_q1,sklep2_q3))
 cat("Wariancja nieobciazona sklepu 2: ", var(dane_sklepu2_vec))
