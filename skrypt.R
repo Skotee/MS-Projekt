ad418dc54abaeb37f09936bb23af43834e94ce9d
DawKub <kubow15@o2.pl>
2016-04-27 19:43:58 +0200

    odchylenie cwiartkowe

--- skrypt.R
+++ skrypt.R
@@ -5,8 +5,14 @@ moda <- function(vec) {
 
 rozstep <- function(vec)
 {
-  ux <- max(vec)-min(vec)
-  return(ux)
+  result <- max(vec)-min(vec)
+  return(result)
+}
+
+cwiartkowe<- function(q1,q3)
+{
+  result<-(q3-q1)/2
+  return(result)
 }
 
 dane_sklepu_1 <- read.table("sklep1.txt", header=F, dec=",")
@@ -20,19 +26,25 @@ dane_sklepu2_vec <- c(dane_sklepu_2[[1]])
 sort(dane_sklepu2_vec)
 
 cat("Œrednia sklepu 1: ", mean(dane_sklepu1_vec))
-cat("Kwantyl 0.25 sklepu 1:", quantile(dane_sklepu1_vec, 0.25))
+sklep1_q1<-quantile(dane_sklepu1_vec, 0.25)
+cat("Kwantyl 0.25 sklepu 1:", sklep1_q1)
 cat("Mediana sklepu 1: ", median(dane_sklepu1_vec))
-cat("Kwantyl 0.75 sklepu 1:", quantile(dane_sklepu1_vec, 0.75))
+sklep1_q3<-quantile(dane_sklepu1_vec, 0.75)
+cat("Kwantyl 0.75 sklepu 1:", sklep1_q3 )
 cat("Odchylenie standardowe sklepu 1: ", sd(dane_sklepu1_vec))
+cat("Odchylenie cwiartkowe sklepu 1: ", cwiartkowe(sklep1_q1,sklep1_q3))
 cat("Wariancja sklepu 1: ", var(dane_sklepu1_vec))
 cat("Dominanta sklepu 1: ", moda(dane_sklepu1_vec))
 cat("Rozstep sklep 1: ", rozstep(dane_sklepu1_vec))
 
 cat("Œrednia sklepu 2: ", mean(dane_sklepu2_vec))
-cat("Kwantyl 0.25 sklepu 2:", quantile(dane_sklepu2_vec, 0.25))
+sklep2_q1<-quantile(dane_sklepu2_vec, 0.25)
+cat("Kwantyl 0.25 sklepu 2:", sklep2_q1 )
 cat("Mediana sklepu 2: ", median(dane_sklepu2_vec))
-cat("Kwantyl 0.75 sklepu 2:", quantile(dane_sklepu2_vec, 0.75))
+sklep2_q3<-quantile(dane_sklepu2_vec, 0.75)
+cat("Kwantyl 0.75 sklepu 2:", sklep2_q3)
 cat("Odchylenie standardowe sklepu 2: ", sd(dane_sklepu2_vec))
+cat("Odchylenie cwiartkowe sklepu 2: ", cwiartkowe(sklep2_q1,sklep2_q3))
 cat("Wariancja sklepu 2: ", var(dane_sklepu2_vec))
 cat("Dominanta sklepu 2: ", moda(dane_sklepu2_vec))
 cat("Rozstep sklep 2: ", rozstep(dane_sklepu2_vec))
